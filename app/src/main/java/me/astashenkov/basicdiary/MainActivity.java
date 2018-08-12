7f4ab59062128bf8e046d4be97e0895b3aae705a
Artem Astashenkov <artem.astashenkov@vartas.fi>
2018-08-12 22:41:42 +0300

    Prepared toolbar icons

--- app/src/main/java/me/astashenkov/basicdiary/MainActivity.java
+++ app/src/main/java/me/astashenkov/basicdiary/MainActivity.java
@@ -86,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        menu.findItem(R.id.action_delete).setVisible(false);
         return true;
     }
 
@@ -99,8 +100,9 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.action_sort) {
+            return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
